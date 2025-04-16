34df60e927f495c0abd961265c77df7c890f8c50
fly <1478312505@qq.com>
2025-04-16 23:30:22 +0800

    登录界面

--- puzzle/src/App.java
+++ puzzle/src/App.java
@@ -1,11 +1,12 @@
 import game.ui.GameJFrame;
+import game.ui.LoginJFrame;
 
 public class App {
     public static void main(String[] args) {
         // 程序的启动入口
         // 想打开谁的界面，只需要创建对象就可以了
-//        new LoginJFrame();
-        new GameJFrame();
+        new LoginJFrame();
+//        new GameJFrame();
 //        new RegisterJFrame();
 
     }
--- puzzle/src/game/ui/LoginJFrame.java
+++ puzzle/src/game/ui/LoginJFrame.java
@@ -5,6 +5,13 @@ import javax.swing.*;
 public class LoginJFrame extends JFrame {
     // 登陆界面
     public LoginJFrame(){
+        // 初始化界面
+        InitJFrame();
+
+        // 初始化视图
+        Initview();
+    }
+    public void InitJFrame(){
         // 在创建对象的时候进行初始化
         this.setSize(488,430);
         this.setTitle("登录");
@@ -13,7 +20,40 @@ public class LoginJFrame extends JFrame {
         this.setLocationRelativeTo(null);
         //关掉界面后停止代码运行
         this.setDefaultCloseOperation(3);
+        // 取消窗口的默认布局管理器
+        this.setLayout(null);
         //显示窗口
         this.setVisible(true);
+    }
+    public void Initview(){
+        // 加载入用户名的图片
+        ImageIcon usernameimage = new ImageIcon("photo/image/login/用户名.png");
+        JLabel usernameText = new JLabel(usernameimage);
+        usernameText.setBounds(116, 135,47,17);
+        this.getContentPane().add(usernameText);
+
+        // 加载入密码的图片
+        ImageIcon passwordimage = new ImageIcon("photo/image/login/密码.png");
+        JLabel passwordText = new JLabel(passwordimage);
+        passwordText.setBounds(130,195,32,16);
+        this.getContentPane().add(passwordText);
+
+        // 加载用户名的输入框
+        JTextField username = new JTextField();
+        username.setBounds(195,134,200,30);
+        this.getContentPane().add(username);
+
+        // 加载密码的输入框
+        JTextField password = new JTextField();
+        password.setBounds(195,195,200,30);
+        this.getContentPane().add(password);
+
+        // 加载背景图片
+        ImageIcon bg = new ImageIcon("photo/image/login/background.png");
+        JLabel background = new JLabel(bg);
+        background.setBounds(0, 0, 470, 390);
+        this.getContentPane().add(background);
+
+        this.getContentPane().repaint();
     }
 }
